b1130e088d44946762bcaef2f47901d888dc14b3
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2024-08-18 22:49:28 +0300

    Get vacancy_urls

--- django/fetcher/views.py
+++ django/fetcher/views.py
@@ -1,25 +1,64 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-# from .models import Vacancy, Company
+from .models import Vacancy
 import uuid
+import os
+import json
+from django.conf import settings
+import time
+
 
 def fetcher(request):
-  config = 'django/fetcher/config.json'
+ # Path to your JSON config file
+  config_path = os.path.join(settings.BASE_DIR, 'fetcher/config.json')
 
+  with open(config_path, 'r') as file:
+      config = json.load(file)  # Load JSON data into a Python dictionary
+
+  # Access the required keys from the dictionary
   keywords_list = config['keywords_list']
-  portal_search_base_urls = config['portal_search_base_urls']
-
+  portals = config['portals']
+  print("portals", portals)
+  # keywords_list = config['keywords_list']
+  # portal_search_base_urls = config['portal_search_base_urls']
   
   for keywords in keywords_list:
-    for search_url in portal_search_base_urls:
-      search_response = requests.get(search_url, params={'keywords[0]': keywords})
+    for _, portal in portals.items():
+      print("portal:", portal)
+      print("portal:", portal['base_url'])
+      print("portal:", portal['search_href'])
+      search_url = portal['base_url'] + portal['search_href']
+      search_response = requests.get(search_url, params={
+        portal['keywords_param']: keywords,
+        portal['limit_param']:1000,
+      })
       vacancy_urls = BeautifulSoup(search_response.content, 'html.parser').find_all('a')
+      print("vacancy_urls: ", len(vacancy_urls), vacancy_urls[17])
 
       # for vacancy_url in vacancy_urls:
-      #   vacancy = requests.get(vacancy_url)
-      #   vacancy_content = vacancy.content.decode("utf-8")
+      #   href = vacancy_url.get('href')
+      #   if not href or not href.startswith(portal["vacancy_base_href"]):
+      #     # Now all hrefs are links to vacancies, some might be links to logos, etc
+      #     continue
+        
+      #   vacancy_url = vacancy_url + href
+      #   if Vacancy.objects.filter(url=vacancy_url).exists():
+      #       print("Skipping - href already exists in the Vacancies table.")
+      #       continue
 
+      #   try:
+      #       vacancy = requests.get(vacancy_url, params=params, headers=headers)
+      #   except IncompleteRead as e:
+      #       print("IncompleteRead: ", e)
+      #       print("Retrying in 5s...")
+      #       time.sleep(5)
+      #       vacancy = requests.get(vacancy_url, params=params, headers=headers)      #   vacancy_content = vacancy.content.decode("utf-8")
+
+      #   vacancy_content = vacancy.content.decode("utf-8")
+      #   print("vacancy_content:\n", vacancy_content)
+      #   return
+        # if vacancy_content: 
       #   # Store company in db
       #   company = Company.update_or_create(
       #       uuid = uuid.uuid4(),
