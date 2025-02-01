7510583c29d0e9b82eff21337cf97f07638a586d
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2025-02-01 05:31:46 -0800

    Add build_files.sh and build.py

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+# python3 -m pip install -r requirements.txt
+
+python3 django_apps/console_tasks/build.py create_ca_pem create_private_settings_json
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Create Vercel-compatible output vercel directory
+mkdir -p .vercel/output/static
+cp -r /vercel/path1/staticfiles/* .vercel/output/static/
+
+python3 manage.py makemigrations
+python3 manage.py migrate
