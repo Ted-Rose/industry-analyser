32c75ea765f575352af816e75a2e69fac41bd0d9
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2025-09-30 19:47:11 +0300

    Make `vacancy_portal_id` unique

--- fetcher/models.py
+++ fetcher/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import models
 import uuid
 
+
 class Vacancy(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company_name = models.CharField(null=True, max_length=255)
@@ -15,7 +16,11 @@ class Vacancy(models.Model):
     first_seen = models.DateTimeField()
     last_seen = models.DateTimeField(null=True)
     days_open = models.IntegerField(null=True)
-    vacancy_portal_id = models.IntegerField(null=True)
+    vacancy_portal_id = models.IntegerField(
+        null=True,
+        unique=True,
+        help_text="The ID of the vacancy on the job portal",
+    )
     application_deadline = models.DateTimeField(null=True)
     state = models.CharField(max_length=50)
 
