46cb652c8ab152135661d0b128f900c587fd7648
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-11-05 10:31:00 +0200

    tv_programs/bugfix/select-related-channels-in-single-query

--- tv_programs/views.py
+++ tv_programs/views.py
@@ -13,6 +13,7 @@ def program_list(request):
     View for listing TV programs with filtering options.
     """
     content_rating = request.GET.get('content_rating', None)
+    not_content_rating = request.GET.get('not_content_rating', None)
     rating_value = request.GET.get('rating_value', None)
     start_date = request.GET.get('start_date', None)
     end_date = request.GET.get('end_date', None)
@@ -23,9 +24,11 @@ def program_list(request):
     query = Q()
 
     if content_rating:
-        query &= Q(content_rating=content_rating)
+        query &= Q(pg_rating=content_rating)
+    if not_content_rating:
+        query &= ~Q(pg_rating=not_content_rating)
     if rating_value:
-        query &= Q(rating_value__gte=float(rating_value))
+        query &= Q(imdb_rating__gte=float(rating_value))
     if start_date:
         query &= Q(start_time__date__gte=start_date)
     if end_date:
@@ -35,7 +38,7 @@ def program_list(request):
     if exclude_channel_id:
         query &= ~Q(channel_id=exclude_channel_id)
 
-    programs = Program.objects.filter(query).order_by('-start_time')
+    programs = Program.objects.select_related('channel').filter(query).order_by('-start_time')
 
     channels = Channel.objects.all()
 
@@ -44,6 +47,7 @@ def program_list(request):
         'channels': channels,
         'filters': {
             'content_rating': content_rating,
+            'not_content_rating': not_content_rating,
             'rating_value': rating_value,
             'start_date': start_date,
             'end_date': end_date,
