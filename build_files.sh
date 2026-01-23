4903663e66fa4152edda27b70bab1377f9da96d9
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-01-23 15:54:38 +0200

    chore: update pip install command to use Python 3.12

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
-uv pip install --system -r requirements.txt
+uv pip install --system -r requirements.txt --python 3.12
 
-uv run python3 industry_analyser/console_tasks/build.py create_ca_pem create_private_settings_json
+uv run --python 3.12 python3 industry_analyser/console_tasks/build.py create_ca_pem create_private_settings_json
 
 # Collect static files
-uv run python3 manage.py collectstatic --noinput
+uv run --python 3.12 python3 manage.py collectstatic --noinput
 
 # Create Vercel-compatible output vercel directory
 mkdir -p .vercel/output/static
 cp -r /vercel/path0/staticfiles/static* .vercel/output/static/
 
-uv run python3 manage.py makemigrations
-uv run python3 manage.py migrate
+uv run --python 3.12 python3 manage.py makemigrations
+uv run --python 3.12 python3 manage.py migrate
