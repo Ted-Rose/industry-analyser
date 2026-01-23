795d8ce5da3a2ae35d313420a638644f7119abe5
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-01-23 15:50:23 +0200

    chore: prefix Python commands with uv run in build script

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
 uv pip install --system -r requirements.txt
 
-python3 industry_analyser/console_tasks/build.py create_ca_pem create_private_settings_json
+uv run python3 industry_analyser/console_tasks/build.py create_ca_pem create_private_settings_json
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+uv run python3 manage.py collectstatic --noinput
 
 # Create Vercel-compatible output vercel directory
 mkdir -p .vercel/output/static
 cp -r /vercel/path0/staticfiles/static* .vercel/output/static/
 
-python3 manage.py makemigrations
-python3 manage.py migrate
+uv run python3 manage.py makemigrations
+uv run python3 manage.py migrate
